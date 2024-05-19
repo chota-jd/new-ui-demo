a0420e71d6184df68a076544218b64a65a527769
Chirag Prajapati <chotajd4444@gmail.com>
2024-05-19 13:19:13 +0530

    fix : login insert data

--- components/FireBaseCrud/FireBaseCrud.tsx
+++ components/FireBaseCrud/FireBaseCrud.tsx
@@ -16,9 +16,11 @@ const database = FirebaseConfig();
 
 function FireBaseCrud() {
     let [username, setUserName] = useState('');
-    let [fullname, setFullName] = useState('');
-    let [phone, setPhone] = useState('');
-    let [dob, setDob] = useState('');
+    let [email, setEmail] = useState('');
+    let [password, setPassWord] = useState('');
+
+
+
 
     const isNullOrWhiteSpaces = (value: string | null | undefined): boolean => {
         if (value == null) {
@@ -27,67 +29,105 @@ function FireBaseCrud() {
         return value.toString().replaceAll(' ', '').length < 1;
     }
 
+
+
     let InsertData = () => {
-        if (isNullOrWhiteSpaces(username)
-        || isNullOrWhiteSpaces(fullname)
-        || isNullOrWhiteSpaces(phone)
-        || isNullOrWhiteSpaces(dob)) {
+        if (isNullOrWhiteSpaces(email) || isNullOrWhiteSpaces(password)) {
             alert("fill all fileds");
             return;
         }
-        set(ref(database, 'Customer/'+username), {
-            fullname: fullname,
-            phonenumber: phone,
-            dateofbirth: dob
+        set(ref(database, 'Customer/' + username), {
+            emaillogin: email,
+            passwordlogin: password
 
         })
     }
 
-let SelectData = () => {
-    const dbref = ref(database);
+    let SelectData = () => {
+        const dbref = ref(database);
 
-    get(child(dbref, 'Customer/' + username)).then(snapshot => {
-        if(snapshot.exists()){
-            setFullName(snapshot.val().fullname);
-            setPhone(snapshot.val().phonenumber);
-            setDob(snapshot.val().dateofbirth);
-        }
+        get(child(dbref, 'Customer/' + email)).then(snapshot => {
+            if (snapshot.exists()) {
+                setPassWord(snapshot.val().passwordlogin);
+            }
 
-        else{
-            alert("no data")
-                }
-    })
-    .catch((error) => {
-        console.log(error);
-        alert("Error data was unsuccessful");
-    })
-}
+            else {
+                alert("no data")
+            }
+        })
+            .catch((error) => {
+                console.log(error);
+                alert("Error data was unsuccessful");
+            })
+    }
     return (
         <>
 
-        <label>Username</label>
+            {/* <label>Username</label>
         <input type="text" value={username} onChange={e=>{setUserName(e.target.value)}}/>
-        <br/>
-
-        <label>Full Name</label>
-        <input type="text" value={fullname} onChange={e=>{setFullName(e.target.value)}}/>
-        <br/>
-
-        <label>Phone Number</label>
-        <input type="text" value={phone} onChange={e=>{setPhone(e.target.value)}}/>
-        <br/>
-
-        <label>Date of Birth</label>
-        <input type="text" value={dob} onChange={e=>{setDob(e.target.value)}}/>
-        <br/>
-
-        <button onClick={InsertData}>Insert Data</button>
-        <button>Update Data</button>
-        <button>Delete Data</button>
-        <button onClick={SelectData}>Select Data</button>
+        <br/> */}
 
 
-    
+
+
+
+
+            <div className="w-full lg:grid lg:min-h-[600px] lg:grid-cols-2 xl:min-h-[800px]">
+                <div className="flex items-center justify-center py-12">
+                    <div className="mx-auto grid w-[350px] gap-6">
+                        <div className="grid gap-2 text-center">
+                            <h1 className="text-3xl font-bold">Login</h1>
+                            <p className="text-balance text-muted-foreground">
+                                Enter your email below to login to your account
+                            </p>
+                        </div>
+                        <div className="grid gap-4">
+                            <div className="grid gap-2">
+                                <Label htmlFor="email">Email</Label>
+                                <Input
+                                    id="email"
+                                    type="email"
+                                    placeholder="m@example.com"
+                                    required
+                                    onChange={e => { setEmail(e.target.value) }}
+
+
+                                />
+
+
+                            </div>
+                            <div className="grid gap-2">
+                                <div className="flex items-center">
+                                    <Label htmlFor="password">Password</Label>
+                                    <Link
+                                        href="/forgot-password"
+                                        className="ml-auto inline-block text-sm underline"
+                                    >
+                                        Forgot your password?
+                                    </Link>
+                                </div>
+                                <Input id="password" type="password" required onChange={e => { setPassWord(e.target.value) }} />
+                            </div>
+                            <Button type="submit" className="w-full" onClick={InsertData}>
+                                Login
+                            </Button>
+                            <Button type="submit" className="w-full" onClick={InsertData}>
+                                Login Edit
+                            </Button>
+                            <Button variant="outline" className="w-full">
+                                Login with Google
+                            </Button>
+                        </div>
+                        <div className="mt-4 text-center text-sm">
+                            Don&apos;t have an account?{" "}
+                            <Link href="#" className="underline">
+                                Sign up
+                            </Link>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
 
 
         </>
